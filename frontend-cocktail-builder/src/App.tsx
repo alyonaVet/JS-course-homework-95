0ad94f15148d9241c3804e4aba1bd9fa7f0d55bc
alyonaVet <a.v.alenka2015@gmail.com>
2024-10-18 11:25:45 +0600

    added all components to App; added routes;

--- frontend-cocktail-builder/src/App.tsx
+++ frontend-cocktail-builder/src/App.tsx
@@ -1,12 +1,40 @@
 import './App.css';
+import AppToolbar from './UI/AppToolbar/AppToolbar';
+import {Route, Routes} from 'react-router-dom';
+import NotFoundPage from './UI/NotFoundPage/NotFoundPage';
+import Register from './features/users/components/Register';
+import Login from './features/users/components/Login';
+import Cocktails from './features/cocktails/Cocktails';
+import AddCocktail from './features/cocktails/AddCocktail';
+import FullOneCocktail from './features/cocktails/FullOneCocktail';
+import UserCocktails from './features/cocktails/UserCocktails';
+import {useAppSelector} from './app/hooks';
+import {selectUser} from './features/users/usersSlice';
+import ProtectedRoute from './UI/ProtectedRoute/ProtectedRoute';
 
 const App = () => {
+  const user = useAppSelector(selectUser);
 
-    return (
-        <>
-            Hello world!
-        </>
-    )
+  return (
+    <>
+      <header>
+        <AppToolbar/>
+      </header>
+      <Routes>
+        <Route path="/" element={<Cocktails/>}/>
+        <Route path="/cocktails/add-cocktail" element={
+          <ProtectedRoute isAllowed={!!user}>
+            <AddCocktail/>
+          </ProtectedRoute>
+        }/>
+        <Route path="/cocktails/:id" element={<FullOneCocktail/>}/>
+        <Route path="/cocktails/my-cocktails" element={<UserCocktails/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="*" element={<NotFoundPage/>}/>
+      </Routes>
+    </>
+  );
 };
 
 export default App;

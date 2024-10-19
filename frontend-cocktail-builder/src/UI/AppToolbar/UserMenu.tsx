812e4be71cba78bc036bcb9cca68f2dddbd538d7
alyonaVet <a.v.alenka2015@gmail.com>
2024-10-19 19:29:42 +0600

    changed avatar check;

--- frontend-cocktail-builder/src/UI/AppToolbar/UserMenu.tsx
+++ frontend-cocktail-builder/src/UI/AppToolbar/UserMenu.tsx
@@ -32,7 +32,7 @@ const UserMenu: React.FC<Props> = ({user}) => {
     navigate('/');
   };
 
-  const avatar = user.avatar?.includes('http') ? user.avatar : `${apiURL}/${user.avatar || ''}`;
+  const avatar = user.avatar?.includes('https') ? user.avatar : `${apiURL}/${user.avatar || ''}`;
 
   return (
     <Box display="flex" alignItems="center">
